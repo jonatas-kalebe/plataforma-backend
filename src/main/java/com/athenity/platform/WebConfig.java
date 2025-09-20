bf6a3b48364a49310833f300e9ab438e7519be45
jonatas-kalebe <jonataskalebe@discente.ufg.br>
2025-09-20 08:56:54 -0300

    Configure CORS for production

--- src/main/java/com/athenity/platform/WebConfig.java
+++ src/main/java/com/athenity/platform/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://plataforma-frontend-peach.vercel.app/")
+                .allowedOrigins("https://plataforma-frontend-peach.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
